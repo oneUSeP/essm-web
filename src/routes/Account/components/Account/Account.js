97d3f0c879097d5f8f7f10c5a734a529c52915c5
Glevinzon D. Dapal <glevinzon.dapal@usep.edu.ph>
2017-11-24 13:30:15 +0800

    added initial styles to pages accounts and entities

--- src/routes/Account/components/Account/Account.js
+++ src/routes/Account/components/Account/Account.js
@@ -4,8 +4,19 @@ import PropTypes from 'prop-types'
 class Account extends Component {
   render () {
     return (
-      <div>
+      <div className='container-fluid container-fluid-spacious' style={{marginTop: '0%'}} >
+        <div className='col-sm-12 content'>
+          <div className='dashhead'>
+            <div className='dashhead-titles'>
+              <h6 className='dashhead-subtitle'>USEP-KMSD | SY 2017-2018</h6>
+              <h3 className='dashhead-title'>Accounts</h3>
+            </div>
+          </div>
+        </div>
 
+        <div className='row'>
+
+        </div>
       </div>
     )
   }
--- src/routes/Table/components/Table/Table.js
+++ src/routes/Table/components/Table/Table.js
@@ -4,8 +4,19 @@ import PropTypes from 'prop-types'
 class Table extends Component {
   render () {
     return (
-      <div>
+      <div className='container-fluid container-fluid-spacious' style={{marginTop: '0%'}} >
+        <div className='col-sm-12 content'>
+          <div className='dashhead'>
+            <div className='dashhead-titles'>
+              <h6 className='dashhead-subtitle'>USEP-KMSD | SY 2017-2018</h6>
+              <h3 className='dashhead-title'>Registration Form Fields</h3>
+            </div>
+          </div>
+        </div>
 
+        <div className='row'>
+
+        </div>
       </div>
     )
   }
